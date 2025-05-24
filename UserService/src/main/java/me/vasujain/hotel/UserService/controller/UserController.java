339ec04f0c46c0a9e12b3c509d20e83a4804408e
vasujain275 <vasujain275@gmail.com>
2025-05-25 01:48:31 +0530

    refactor: updated UserService Controller

--- UserService/src/main/java/me/vasujain/hotel/UserService/controller/UserController.java
+++ UserService/src/main/java/me/vasujain/hotel/UserService/controller/UserController.java
@@ -10,13 +10,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
-@Controller
+@RestController
 @RequestMapping("/users")
 @AllArgsConstructor
 public class UserController {
