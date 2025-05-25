476453e97d0545390190430f6e907aca385155f9
vasujain275 <vasujain275@gmail.com>
2025-05-25 13:06:03 +0530

    feat: Added Eureka Service Registory

---
+++ ServiceRegistory/src/test/java/me/vasujain/hotel/ServiceRegistory/ServiceRegistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package me.vasujain.hotel.ServiceRegistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
