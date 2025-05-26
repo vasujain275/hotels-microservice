72485eea99401ebe4dd17195ba4b71fc83e6b117
vasujain275 <vasujain275@gmail.com>
2025-05-26 19:27:18 +0530

    feat: Added Api Gateway

---
+++ ApiGateway/src/test/java/me/vasujain/hotel/ApiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package me.vasujain.hotel.ApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
